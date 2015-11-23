c3b41e4bc65144e76ac10f0573fd2d412822eb5d
chrishsu <chia-hao.hsu@aiesec.net>
2015-11-23 18:34:25 -0500

    Modify server program to GAE version

--- unary.py
+++ unary.py
@@ -1,19 +1,26 @@
-from flask import Flask
-from flask import render_template, url_for
+import jinja2
+import os
+import webapp2
 
-app = Flask(__name__)
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env    = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True )
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class Handler(webapp2.RequestHandler):
+  def write(self, *a, **kw):
+      self.response.out.write(*a, **kw)
 
-@app.route('/unary/')
-@app.route('/unary/index')
-def unary():
-    # url_for('static', filename='style.css')
-    # url_for('static', filename='test.json')
-    # url_for('static', filename='site.js')
-    return render_template('index.html')
+  def render_str(self, template, **params):
+      template = jinja_env.get_template(template)
+      return template.render(params)
 
-if __name__ == '__main__':
-    app.run()
+  def render(self, template, **kw):
+      self.write(self.render_str(template, **kw))
+
+class UnaryPage(Handler):
+    def get(self):
+        self.render("index.html")
+
+app = webapp2.WSGIApplication([
+    ('/', UnaryPage),
+], debug=True)
+
