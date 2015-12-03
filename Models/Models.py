d6615f5303bf9992dcbe3d6ad47516252a4d2c45
chrishsu <chia-hao.hsu@aiesec.net>
2015-12-02 23:49:43 -0500

    Add Event DB

--- Models/Models.py
+++ Models/Models.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python
 
+from django.utils import simplejson
 from google.appengine.ext import db
+
+def event_key(group='default'):
+    return db.Key.from_path('event', group)
 
 # These classes define the data objects
 # that you will be able to store in
 # AppEngine's data store.
+class JsonProperty(db.TextProperty):
+    def validate(self, value):
+        return value
+
+    def get_value_for_datastore(self, model_instance):
+        result = super(JsonProperty, self).get_value_for_datastore(model_instance)
+        result = simplejson.dumps(result)
+        return db.Text(result)
+
+    def make_value_from_datastore(self, value):
+        try:
+            value = simplejson.loads(str(value))
+        except:
+            pass
+
+        return super(JsonProperty, self).make_value_from_datastore(value)
 
 class Event(db.Model):
     def __init__(self):
-        name = db.StringProperty(required = True)
-        pw_hash = db.StringProperty(required = True)
-        email = db.StringProperty()
+        event_id = StringProperty(required=True)
+        event_json = JsonProperty()
 
-    def by_id(self, uid):
-        return self.get_by_id(uid, parent = users_key())
+    @classmethod
+    def by_id(cls, event_id):
+        return Event.get_by_id(event_id, parent=event_key())
+
+    @classmethod
+    def create(cls, event_json):
+        new_event = Event(parent=event_key(), event_json=event_json)
+        new_event.put()
