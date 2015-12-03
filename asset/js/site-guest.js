557e6f2620a3988132cfb019b99c744be3d417ac
Sun-Yi Lin <lsphate@gmail.com>
2015-12-03 18:23:38 -0500

    remove all -fluid class

--- asset/js/site-guest.js
+++ asset/js/site-guest.js
@@ -1,9 +1,9 @@
-// events = JSON.parse(events);
+// Prepare variables
 var divSize = ($('div.container').width())/12*7;
 $('#svg_donut').width(divSize).height(divSize);
 
 var str = $('input#response').val(),
-    testJSON = $.parseJSON(str),
+    testJSON = $.parseJSON(str);
 
 var eventName = testJSON["describe"],
     hostname = testJSON["hostusr"],
@@ -55,8 +55,6 @@ for (i = 0; i < guests.length; i++) {
     }
 
 }
-
-
 
 
 
